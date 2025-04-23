b92a08e0e43f09f5a301189af025658d71aad0dd
LuanMendesMoura <luanmendesmoura091207@gmail.com>
2025-04-23 18:20:56 -0400

    falta algumas estilizacoes mas esta pronto

--- Exercicio-2/components/artigo.php
+++ Exercicio-2/components/artigo.php
@@ -3,14 +3,14 @@
     function artigo($faviconSite, $nomeSite, $tituloArtigo, $textoArtigo, $imageArtigo) {
         return 
         "<article class='artigo'>
-            <div class='div-group'>
+            <div class='group-left'>
                 <div class='group-top'>
                     <img src='$faviconSite' alt='favicon'>
-                    <h3>$nomeSite</h3>
+                    <h3 class='artigo-h3'>$nomeSite</h3>
                 </div>
-                <div class='group-left'>
-                    <h1>$tituloArtigo</h1>
-                    <p>$textoArtigo</p>
+                <div class='group-bottom'>
+                    <h1 class='artigo-h1'>$tituloArtigo</h1>
+                    <p class='artigo-p'>$textoArtigo</p>
                 </div>
             </div>
             <div class='group-right'>
--- Exercicio-2/index.php
+++ Exercicio-2/index.php
@@ -11,6 +11,8 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?= artigo("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAY1BMVEXWIiLWHx/VGxvXKSndVVXUAAD77OzZOjrVGhrfY2P////VFBTkgYHUCwvVBATmi4vdWVngamrwu7v33NzZPT344eHqo6PhcHDtsLD21tbyxsbje3vYLi7cTk7tq6vqn5/bSEhIqvieAAAAeUlEQVR4Ad2O1wHCMAxEZYMK5/Tek/2nZAAUBsj7vUoPJcQ/2ut9r7JouNPsAyZKvpgyaF6UrhpTBamblj2t6hg90LBzqmiGEdOMkT+/Ki8A2hXltrCThGDaATii7agHIzlQJ2e07DlElpPJIdcxbuXF5BKMQor0SL6UzwVRfRSkJwAAAABJRU5ErkJggg==","Laravel Pro Tips","Laravel Pro Tips - The best tweets & articles about Laravel!","Here's a pro tip for all you Laravel developers: You can customize the default order of data retrieved from relationships!","https://pbs.twimg.com/media/FOG28lHUcAAk6Om.jpg?name=medium") ?>
+    <main class="main-artigo">
+        <?= artigo("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAY1BMVEXWIiLWHx/VGxvXKSndVVXUAAD77OzZOjrVGhrfY2P////VFBTkgYHUCwvVBATmi4vdWVngamrwu7v33NzZPT344eHqo6PhcHDtsLD21tbyxsbje3vYLi7cTk7tq6vqn5/bSEhIqvieAAAAeUlEQVR4Ad2O1wHCMAxEZYMK5/Tek/2nZAAUBsj7vUoPJcQ/2ut9r7JouNPsAyZKvpgyaF6UrhpTBamblj2t6hg90LBzqmiGEdOMkT+/Ki8A2hXltrCThGDaATii7agHIzlQJ2e07DlElpPJIdcxbuXF5BKMQor0SL6UzwVRfRSkJwAAAABJRU5ErkJggg==","Laravel Pro Tips","Laravel Pro Tips - The best tweets & articles about Laravel!","Here's a pro tip for all you Laravel developers: You can customize the default order of data retrieved from relationships!","https://pbs.twimg.com/media/FOG28lHUcAAk6Om.jpg?name=medium") ?>
+    </main>
 </body>
 </html>
